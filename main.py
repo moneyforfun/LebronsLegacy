6c351edee7a44b6e8fc9514798e0b29020642d22
moneyforfun <konradh.ty@gmail.com>
2023-12-22 20:17:34 +0100

    code&csv update

--- main.py
+++ main.py
@@ -2,28 +2,31 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-years = list(range(1991,2022))
+years = list(range(1991,2021))
 url_start = 'https://www.basketball-reference.com/awards/awards_{}.html'
 
-for year in years:
+'''for year in years:
     url = url_start.format(year)
     data = requests.get(url)
 
     with open('mvp/{}.html'.format(year),'w+', encoding="utf-8") as f:
-        f.write(data.text)
-
+        f.write(data.text)'''
+dfs = []
 for year in years:
-    dfs = []
-    with open('mvp/{}.html'.format(year)) as f:
-        page=f.read()
+    with open('mvp/{}.html'.format(year), encoding="utf-8") as f:
+        page=f.read( )
     soup = BeautifulSoup(page,'html.parser')
-    #soup.find('tr', class_='over_header').decompose()
+    soup.find('tr', class_='over_header').decompose()
     mvp_table = soup.find(id='mvp')
     mvp = pd.read_html(str(mvp_table))[0]
+    mvp['Year'] = year
+
+
     dfs.append(mvp)
 mvps = pd.concat(dfs)
 
 mvps.to_csv('mvps.csv')
+
 
 
 
