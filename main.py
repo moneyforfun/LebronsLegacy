bc1b375a4f70811a80eea15edc9886e51b13ba78
moneyforfun <konradh.ty@gmail.com>
2023-12-23 23:59:35 +0100

    code&csv update

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ url_start = 'https://www.basketball-reference.com/awards/awards_{}.html'
 
     with open('mvp/{}.html'.format(year),'w+', encoding="utf-8") as f:
         f.write(data.text)'''
-dfs = []
+'''dfs = []
 for year in years:
     with open('mvp/{}.html'.format(year), encoding="utf-8") as f:
         page=f.read( )
@@ -20,15 +20,26 @@ for year in years:
     mvp_table = soup.find(id='mvp')
     mvp = pd.read_html(str(mvp_table))[0]
     mvp['Year'] = year
-
-
     dfs.append(mvp)
 mvps = pd.concat(dfs)
+'''
+'''mvps.to_csv('mvps.csv')'''
+player_stats_url = 'https://www.basketball-reference.com/leagues/NBA_{}_per_game.html'
 
-mvps.to_csv('mvps.csv')
+from selenium import webdriver
+driver = webdriver.Chrome()
+import time
+
+for year in years:
+    url = player_stats_url.format(year)
+
+    driver.get(url)
+    driver.execute_script('window.scrollTo(1,10000)')
+    time.sleep(2)
+
+    html = driver.page_source
+    with open('player/{}.html'.format(year),'w+',encoding="utf-8") as f:
+        f.write(html)
 
 
 
-
-'''soup = BeautifulSoup(page,'html.parser')
-soup.find_all(id='div_pgl_basic')'''
