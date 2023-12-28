5ab3de9b18b14c9b872ee617f32a2622879a9cc2
moneyforfun <konradh.ty@gmail.com>
2023-12-28 01:51:19 +0100

    code&csv update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-years = list(range(1991, 2022))
+years = list(range(1991, 2021))
 url_start = 'https://www.basketball-reference.com/awards/awards_{}.html'
 
 '''for year in years:
@@ -27,6 +27,7 @@ mvps = pd.concat(dfs)
 player_stats_url = 'https://www.basketball-reference.com/leagues/NBA_{}_per_game.html'
 
 from selenium import webdriver
+
 driver = webdriver.Chrome()
 import time
 
@@ -54,35 +55,34 @@ for year in years:
 players = pd.concat(dfs)
 players.to_csv('players.csv')'''
 
-#division scraping
+# division scraping
 '''team_stats_url = 'https://www.basketball-reference.com/leagues/NBA_{}_standings.html'
 for year in years:
-        url = team_stats_url.format(year)
-        data = requests.get(url)
-        with open('team/{}.html'.format(year),'w+',encoding="utf-8") as f:
-                f.write(data.text)'''
+    url = team_stats_url.format(year)
+    data = requests.get(url)
+    with open('team/{}.html'.format(year), 'w+', encoding="utf-8") as f:
+        f.write(data.text)'''
 dfs = []
 for year in years:
-        with open('team/{}.html'.format(year),'r+',encoding="utf-8") as f:
-                page = f.read()
-        soup = BeautifulSoup(page, 'html.parser')
-        soup.find('tr', class_='thead').decompose()
-        team_table = soup.find(id='divs_standings')
-        team = pd.read_html(str(team_table))[0]
-        team['Year'] = year
-        team['Team'] = team['Eastern Conference']
-        del team['Eastern Conference']
-        dfs.append(team)
+    with open('team/{}.html'.format(year), 'r+', encoding="utf-8") as f:
+        page = f.read()
 
-        soup = BeautifulSoup(page, 'html.parser')
-        soup.find('tr', class_='thead').decompose()
-        team_table = soup.find(id='divs_standings')
-        team = pd.read_html(str(team_table))[0]
-        team['Year'] = year
-        team['Team'] = team['Western Conference']
-        del team['Western Conference']
-        dfs.append(team)
+    soup = BeautifulSoup(page, 'html.parser')
+    soup.find('tr', class_='thead').decompose()
+    team_table = soup.find(id='divs_standings_E')
+    team = pd.read_html(str(team_table))[0]
+    team['Year'] = year
+    team['Team'] = team['Eastern Conference']
+    del team['Eastern Conference']
+    dfs.append(team)
+
+    soup = BeautifulSoup(page, 'html.parser')
+    soup.find('tr', class_='thead').decompose()
+    team_table = soup.find(id='divs_standings_W')
+    team = pd.read_html(str(team_table))[0]
+    team['Year'] = year
+    team['Team'] = team['Western Conference']
+    del team['Western Conference']
+    dfs.append(team)
 teams = pd.concat(dfs)
-print(teams)
-
-
+teams.to_csv('teams.csv')
